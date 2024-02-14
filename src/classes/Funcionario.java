87e29d77fd0bfef7452ab8c484735709361f3b57
Ana Helena Peres <110307085+anahperes@users.noreply.github.com>
2024-02-14 20:38:24 -0300

    adicionada interface e classe funcionario

---
+++ src/classes/Funcionario.java
@@ -0,0 +1,13 @@
+package classes;
+
+import Interface.IFuncionario;
+
+public class Funcionario implements IFuncionario {
+    private String nome;
+    private double salario;
+
+    @Override
+    public void promover() {
+
+    }
+}
