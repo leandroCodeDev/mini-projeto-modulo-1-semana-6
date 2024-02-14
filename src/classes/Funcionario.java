d5d5f21722543d2ac2f6fc8c99732f4483676137
Ana Helena Peres <110307085+anahperes@users.noreply.github.com>
2024-02-14 20:48:15 -0300

    adicionada heranca de funcionario em professor.

--- src/classes/Funcionario.java
+++ src/classes/Funcionario.java
@@ -10,4 +10,25 @@ public class Funcionario implements IFuncionario {
     public void promover() {
 
     }
+
+    public Funcionario(String nome, Double salario) {
+        this.nome = nome;
+        this.salario = salario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
 }
