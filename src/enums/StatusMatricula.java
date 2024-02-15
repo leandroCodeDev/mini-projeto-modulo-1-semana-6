cabc28deb664405dfb882b7e86aee0d76fbef952
Breno Rippel <39842293+BrenoRippel@users.noreply.github.com>
2024-02-14 23:05:33 -0300

    criados métodos que demonstram o uso de values() e valueOf().

--- src/enums/StatusMatricula.java
+++ src/enums/StatusMatricula.java
@@ -2,17 +2,32 @@ package enums;
 
 public enum StatusMatricula {
 
-    ATIVO("Ativo"),
-    TRANCADO("Trancado"),
-    FORMADO("Formado");
+    ATIVO("Ativo", "Calouro"),
+    TRANCADO("Trancado", "Preguiçoso"),
+    FORMADO("Formado", "Guerreiro");
 
-    private final String label;
+    private final String situacao;
+    private final String descricao;
 
-    StatusMatricula(String label) {
-        this.label = label;
+    StatusMatricula(String situacao, String descricao) {
+        this.situacao = situacao;
+        this.descricao = descricao;
     }
 
-    public String getLabel(){
-        return label;
+    public String getSituacao(){
+        return situacao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public static StatusMatricula getDescricaoPelaSituacao(String situacao){
+        for (StatusMatricula value : values()) {
+            if(value.situacao == situacao){
+                return value;
+            }
+        }
+        return null;
     }
 }
